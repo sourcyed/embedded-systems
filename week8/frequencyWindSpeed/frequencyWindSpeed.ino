c194becc49f55f280f6d8c52d664495b9a43024b
Sylvi Kokko <sylvi.kokko@tuni.fi>
2023-03-06 11:24:06 +0200

    Changed some pins and added the keypad

--- week8/frequencyWindSpeed/frequencyWindSpeed.ino
+++ week8/frequencyWindSpeed/frequencyWindSpeed.ino
@@ -1,17 +1,32 @@
 #include <LiquidCrystal.h>
+#include <Keypad.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 37, en = 36, d4 = 35, d5 = 34, d6 = 33, d7 = 32;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int buttonPin = 2; // Pin connected to the button
+int buttonPin = 22; // Pin connected to the button
 volatile unsigned long lastTimePressed = 0;
 volatile double frequency = 0;
 
-volatile bool isWindSpeed = true;
+const byte ROWS = 4; 
+const byte COLS = 4; 
 
-const int frequencyInput = 3;
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+volatile bool isWindSpeed = true;
+const int frequencyInput = 23;
 
 float windSpeed = 0;
 
